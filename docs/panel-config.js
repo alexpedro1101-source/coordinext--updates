9da40cb4501d49b415f4c54e31a460c4fdaffcf6
alexpedro1101-source <alexpedro1101@gmail.com>
2026-08-01 00:24:03 -0500

    Add files via upload

--- docs/panel-config.js
+++ docs/panel-config.js
@@ -3,5 +3,5 @@ window.COORDINEXT_PANEL_CONFIG = Object.freeze({
   "supabasePublishableKey": "sb_publishable_cquX5xKcz0BZkS7M5u3CvA_M6zAZYQ9",
   "adminFunctionName": "admin-users",
   "refreshSeconds": 20,
-  "version": "10.6.1"
+  "version": "11.0.1"
 });
